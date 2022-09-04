b90ac0c515172b7b91a8d56d76c95f05eccfcd5a
Ilua Kisialiou <iluxa91@inbox.ru>
2022-09-04 15:49:46 +0300

    part3.2 css refactoring, button added

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,68 +6,68 @@ import Fade from "react-reveal/Fade";
 import ReactTypingEffect from "react-typing-effect";
 import Tilt from 'react-tilt'
 
+const particleOptions = {
+    fullScreen: {enable: true},
+    fpsLimit: 120,
+    interactivity: {
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.2,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.2,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: {min: 1, max: 5},
+        },
+    },
+    detectRetina: true,
+}
+
 export const Main = () => {
     const particlesInit = useCallback(async (engine) => {
         await loadFull(engine);
     }, []);
-
-    const particleOptions = {
-        fullScreen: {enable: true},
-        fpsLimit: 120,
-        interactivity: {
-            modes: {
-                push: {
-                    quantity: 4,
-                },
-                repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                },
-            },
-        },
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.2,
-                width: 1,
-            },
-            collisions: {
-                enable: true,
-            },
-            move: {
-                directions: "none",
-                enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800,
-                },
-                value: 80,
-            },
-            opacity: {
-                value: 0.2,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: {min: 1, max: 5},
-            },
-        },
-        detectRetina: true,
-    }
 
     return (
         <div id={'home'} className={s.mainBlock}>
@@ -85,9 +85,9 @@ export const Main = () => {
                     </div>
                     <div className={s.photo}>
                         <Tilt className="Tilt" options={{ max : 25 }}>
-                        <div className={s.image}></div>
+                        <div className={s.image}/>
                         </Tilt>
-                        <div className={s.baget}></div>
+                        <div className={s.baget}/>
                     </div>
                 </div>
             </Fade>
