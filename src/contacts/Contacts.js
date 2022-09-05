7d9983d241dbd15a7841067122b48e90561b8723
Ilua Kisialiou <iluxa91@inbox.ru>
2022-09-05 19:28:36 +0300

    contact form added

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,7 +1,20 @@
 import s from "./Contacts.module.scss"
 import Fade from "react-reveal/Fade";
+import {useForm} from "react-hook-form";
+import axios from "axios";
 
 export const Contacts = () => {
+    const {register, formState: {errors}, handleSubmit} = useForm();
+
+    const onSubmit = (data, e) => {
+        e.preventDefault()
+        axios.post('https://smtp-server-mails.herokuapp.com/sendMessage', {data})
+            .then((res) => {
+                alert('Your message has been send');
+            });
+        e.target.reset()
+    }
+
     return (
         <div id={'contact'} className={s.contactsBlock}>
             <div className={s.container}>
@@ -10,10 +23,15 @@ export const Contacts = () => {
                 <h2>Contact</h2>
                 </Fade>
                 <Fade bottom>
-                <form className={s.contactForm}>
-                    <input type='text' placeholder={'Your Name *'} className={s.input}/>
-                    <input type='text' className={s.input} placeholder={'Your Email *'}/>
-                    <textarea className={s.textArea} placeholder={'Your message...'}/>
+                <form onSubmit={handleSubmit(onSubmit)} id={'contact-form'} className={s.contactForm}>
+                    <input {...register('name', {required: true})} type='text' placeholder={'Your Name *'} className={s.input}/>
+                    {errors.name && <p style={{color: '#fff'}}>Name is required</p>}
+                    <input {...register('email', {
+                        required: true,
+                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                    })} type='text' className={s.input} placeholder={'Your Email *'}/>
+                    {errors.email && <p style={{color: '#fff'}}>E-mail is required</p>}
+                    <textarea {...register('message', {required: true})} className={s.textArea} placeholder={'Your message...'}/>
                     <button type='submit' className={s.submitBtn}>SEND MESSAGE</button>
                 </form>
                 </Fade>
