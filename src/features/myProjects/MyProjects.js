77834dbfe2a463139b5350be873420fe382384d7
Ilua Kisialiou <iluxa91@inbox.ru>
2022-10-09 20:59:57 +0300

    swiper added

--- src/features/myProjects/MyProjects.js
+++ src/features/myProjects/MyProjects.js
@@ -4,18 +4,56 @@ import {Title} from "../../common/components/title/Title";
 import Fade from "react-reveal/Fade";
 import {projects} from "./";
 
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { Keyboard, Scrollbar, Navigation, Pagination } from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
+
 
 export const MyProjects = () => (
-        <div id={"projects"} className={s.projectsBlock}>
-            <div className={s.projectsContainer}>
+    <div id={"projects"} className={s.projectsBlock}>
+        <div className={s.projectsContainer}>
+            <Fade bottom>
+                <Title spanText={"Portfolio"} headText={"My Projects"}/>
+            </Fade>
+            <div className={s.projects}>
                 <Fade bottom>
-                    <Title spanText={"Portfolio"} headText={"My Projects"}/>
+                    <Swiper
+                        slidesPerView={1}
+                        centeredSlides={false}
+                        slidesPerGroupSkip={1}
+                        grabCursor={true}
+                        keyboard={{
+                            enabled: true,
+                        }}
+                        breakpoints={{
+                            769: {
+                                slidesPerView: 2,
+                                slidesPerGroup: 2,
+                            },
+                        }}
+                        scrollbar={true}
+                        navigation={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+                        className="mySwiper"
+                    >
+                        {projects.map(p => <SwiperSlide><OneProject style={p.style}
+                                                                    title={p.title}
+                                                                    description={p.description}
+                                                                    projectPath={p.projectPath}
+                                                                    codePath={p.codePath}/>
+                        </SwiperSlide>)}
+
+                    </Swiper>
                 </Fade>
-                <div className={s.projects}>
-                    <Fade bottom>
-                        {projects.map(p=><OneProject style={p.style} title={p.title} description={p.description} projectPath={p.projectPath} codePath={p.codePath}/>) }
-                    </Fade>
-                </div>
             </div>
         </div>
-    )
+    </div>
+)
